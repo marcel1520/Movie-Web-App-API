3e78baf90b5bbc0c92bc8b4bb66b5a632aa86799
Marcel Mann <you@example.com>
2025-05-08 23:51:31 +0200

    Movie Web App API

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, render_template, url_for, redirect, flash
+from data_manager import SQLiteDataManager
+from fetch_movie_data import fetch_movie_info
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+data_manager = SQLiteDataManager(app)
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        db_users = data_manager.get_all_users()
+        return render_template('users.html', users=db_users)
+
+    elif request.method == 'POST':
+        name = request.form['name']
+        data_manager.add_user({'name': name})
+        return redirect(url_for('users'))
+
+
+@app.route('/users/<int:user_id>/movies', methods=['GET', 'POST'])
+def user_movies(user_id):
+    if request.method == 'POST':
+        title = request.form.get('title')
+        if title:
+            movie_data = fetch_movie_info(title)
+            if movie_data:
+                data_manager.add_movie(user_id, movie_data)
+            else:
+                flash("Movie not found", "Error")
+        else:
+            flash("Provide title", "Warning")
+        return redirect(url_for('user_movies', user_id=user_id))
+
+    user = data_manager.get_user(user_id)
+    movies = data_manager.get_user_movies(user_id)
+    return render_template('user_movies.html', user=user, movies=movies)
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    data_manager.delete_movie(user_id, movie_id)
+    return redirect(url_for('user_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    data_manager.delete_user(user_id)
+    return redirect(url_for('users'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ fetch_movie_data.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+
+load_dotenv(".env")
+api_key = os.getenv("API_KEY")
+
+
+def serialize_data(data):
+    movie_data = {
+        'title': data['Title'],
+        'release_year': data['Year'],
+        'genre': data['Genre'],
+        'director': data['Director'],
+        'rating': data['imdbRating'],
+        'poster_url': data['Poster']
+    }
+    return movie_data
+
+
+def fetch_movie_info(title):
+    url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
+    res = requests.get(url)
+    data = res.json()
+    return serialize_data(data)
+
+
+
+
